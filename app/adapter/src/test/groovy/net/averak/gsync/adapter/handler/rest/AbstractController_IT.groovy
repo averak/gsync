d6efbd6722287030f59ed5854cad441bd0b005ac
averak <averak.jp@gmail.com>
2024-01-12 06:49:44 +0900

    refs #53 RestTesterのパスを変更

--- app/adapter/src/test/groovy/net/averak/gsync/adapter/handler/rest/AbstractController_IT.groovy
+++
@@ -1,7 +0,0 @@
-package net.averak.gsync.adapter.handler.rest
-
-import net.averak.gsync.testkit.AbstractDatabaseSpec
-
-abstract class AbstractController_IT extends AbstractDatabaseSpec {
-
-}
--- testkit/src/main/kotlin/net/averak/gsync/testkit/api/rest/RequestBuilder.kt
+++ testkit/src/main/kotlin/net/averak/gsync/testkit/api/rest/RequestBuilder.kt
@@ -1,4 +1,4 @@
-package net.averak.gsync.testkit.api
+package net.averak.gsync.testkit.api.rest
 
 import net.averak.gsync.adapter.handler.rest.GlobalRestControllerAdvice
 import net.averak.gsync.adapter.handler.rest.HttpRequestScope
